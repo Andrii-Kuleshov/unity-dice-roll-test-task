60ac7009de79ae8d022d8a194cdb321e07976281
Andrii Kuleshov <andreiandreik19@gmail.com>
2026-06-03 16:11:01 +0300

    Added attack button and dice roll trigger

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -5,11 +5,11 @@ public class DiceRoll : MonoBehaviour
     int strengthModifier = 3;
     int questDifficulty = 12;
 
-    void Start()
+    public void RollDice()
     {
         int diceRoll = Random.Range(1, 21);
         int totalResult = diceRoll + strengthModifier;
-        
+
         Debug.Log("Dice Roll: " + diceRoll);
         Debug.Log("Modifier: " + strengthModifier);
         Debug.Log("Total Result: " + totalResult);
